84cbf3599fd5a2ac4c29f1b30c641d3553de1b03
Ilja Orlovs <vrghost@gmail.com>
2023-12-25 12:02:55 +0000

    send_stream

--- bin/send_stream.sh
+++ bin/send_stream.sh
@@ -3,12 +3,11 @@
 # send local video 
 
 TARGET_DEVICE="${1:-/dev/video0}"
-TARGET_IP="${2:-$(echo "${SSH_CLIENT}" | cut -d ' ' -f 1)}"
+TARGET_HOST="${2:-$(echo "${SSH_CLIENT}" | cut -d ' ' -f 1)}:1234"
 
 ffmpeg \
     -f v4l2 -i "${TARGET_DEVICE}" \
     -c:v libx264 -pix_fmt yuv420p \
-    -framerate 15 -g 30 -b:v 500k \
-    c:a aac -b:a 128k -ar 44100 -ac 2 \
+    -framerate 5 \
     -preset ultrafast -tune zerolatency \
-    -f mpegts "udp://${TARGET_IP}:1234"
+    -f mpegts "udp://${TARGET_HOST}"
