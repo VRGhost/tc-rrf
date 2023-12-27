d31e4b7d8936a3556b425e335ba31f6587ae7883
Ilja Orlovs <vrghost@gmail.com>
2023-12-27 21:11:51 +0000

    latency reduction

--- bin/send_stream.sh
+++ bin/send_stream.sh
@@ -8,6 +8,7 @@ TARGET_HOST="${2:-$(echo "${SSH_CLIENT}" | cut -d ' ' -f 1)}:1234"
 ffmpeg \
     -f v4l2  \
     -i "${TARGET_DEVICE}" \
+    -fflags nobuffer -flags low_delay \
     -codec h264 \
     -filter:v fps=5 \
     -c:v libx264 -pix_fmt yuv420p \
